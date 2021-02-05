ecf9d7a6988f9b084cdcbc4f021a1e1e22ff4c0d
Cassandra <cparisi1290@gmail.com>
2021-02-05 17:11:33 -0500

    removed instance variable for user in redirect route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,9 +24,14 @@ class SessionsController < ApplicationController
         user = User.find_by_email(params[:email])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
-            redirect "/users/#{@user.id}"
+            redirect "/users/#{user.id}"
         else
             redirect '/login'
         end
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/'
     end
 end
