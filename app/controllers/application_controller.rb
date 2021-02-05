9f7148c25c7ec27974f559a32eccc0d839b58ddb
Cassandra <cparisi1290@gmail.com>
2021-02-05 17:27:01 -0500

    enable sessions and session secret

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,18 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "easy-move"
   end
 
   get "/" do
     erb :welcome
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.find
+    end
   end
 
 end
