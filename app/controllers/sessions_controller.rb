41adb96f41a576fe6dfb6db5c9735352c0d5eed6
Cassandra <cparisi1290@gmail.com>
2021-02-05 16:44:44 -0500

    add post /signup route, saved new user(params), redirect to user dashboard

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,16 @@
 class SessionsController < ApplicationController
-    
+
     get '/signup' do
         erb :"sessions/signup"
+    end
+
+    post '/users' do
+            # create new user
+        @user = User.new(params)
+            # save new user
+        @user.save
+            # create new session to remember user
+        session[:user_id] = @user.id
+        redirect "/users/#{@user.id}"
     end
 end
