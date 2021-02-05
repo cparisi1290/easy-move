fbf92754f965084b1ff70f1d5dcbb90d6cb29322
Cassandra <cparisi1290@gmail.com>
2021-02-05 17:51:18 -0500

    write out the the seven routes solely, write out index route to get all blog posts from db

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,3 +1,33 @@
 class BlogPostsController < ApplicationController
 
+    # READ display all blog posts
+    get '/blog_posts' do
+        @blog_posts = BlogPost.all
+        erb :"blog_posts/index"
+    end
+
+    # CREATE display form
+    get '/blog_posts/new' do
+
+    end
+
+    # CREAT new blog post from user
+    post '/blog_posts' do
+
+    end
+
+    # UPDATE display form
+    get '/blog_posts' do
+
+    end
+
+    # UPDATE 
+    patch '/blog_posts' do
+
+    end
+
+
+    delete '/blog_posts' do
+
+    end
 end
