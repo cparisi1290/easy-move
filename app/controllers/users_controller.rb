ec8ac5be3a1ba5a8c844ab83f7f2054d3d2a60b1
Cassandra <cparisi1290@gmail.com>
2021-02-05 16:53:02 -0500

    create user's dashboard by finding user id and rendering their show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,8 @@
 class UsersController < ApplicationController
 
+    get '/users/:id' do
+        @user = User.find(params["id"])
+        # find user id to display their show page
+        erb :"users/show"
+    end
 end
