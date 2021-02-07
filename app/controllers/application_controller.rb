dd706828670fccff7bfec98371d7b668b1ea3968
Cassandra <cparisi1290@gmail.com>
2021-02-07 09:46:08 -0500

    create helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,18 @@ class ApplicationController < Sinatra::Base
   end
 
   helpers do
+
+    def logged_in?
+      !!current_user
+    end
+
     def current_user
-      @current_user ||= User.find
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      #    if @current_user
+        #      @current_user 
+        #    else 
+        #     @current_user =  User.find(session[:user_id])
+        #    end
     end
   end
 
