0c8c087d09bd1de61119549019dffd6918306d7c
Cassandra <cparisi1290@gmail.com>
2021-02-08 09:45:35 -0500

    create blog post index route & blog post new route

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,17 +8,27 @@ class BlogPostsController < ApplicationController
 
     # CREATE display form
     get '/blog_posts/new' do
-
+        if logged_in?
+            erb :"blog_posts/new"
+        else 
+            redirect '/login'
+        end
     end
 
-    # CREAT new blog post from user
+    # CREAT new blog post from current logged in user
     post '/blog_posts' do
-
+        if logged_in?
+            blog_post = current_user.blog_posts.build(params)
+            blog_post.save
+            redirect '/blog_posts'
+        else
+            redirect '/login'
+        end
     end
 
     # READ display one particular blog post show page
     get '/blog_posts/:id' do
-        
+
     end
 
     # UPDATE display form
