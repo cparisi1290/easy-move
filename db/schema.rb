dfc64d9861921572e4710cbc140e968dd663d3b3
Cassandra <cparisi1290@gmail.com>
2021-02-08 17:42:06 -0500

    create migrations for items, categories, and boxes tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_210331) do
+ActiveRecord::Schema.define(version: 2021_02_08_211335) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "boxes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "box_id"
+    t.integer "cateogry_id"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
