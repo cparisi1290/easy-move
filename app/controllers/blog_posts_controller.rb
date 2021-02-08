12980701581f1eb2c3a5026711c4efff866f02b8
Cassandra <cparisi1290@gmail.com>
2021-02-08 12:05:21 -0500

    create authorization method to refactor

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,48 +2,37 @@ class BlogPostsController < ApplicationController
 
     # READ display all blog posts
     get '/blog_posts' do
+        redirect_if_not_logged_in
         @blog_posts = BlogPost.all
         erb :"blog_posts/index"
     end
 
     # CREATE display form
     get '/blog_posts/new' do
-        if logged_in?
-            erb :"blog_posts/new"
-        else 
-            redirect '/login'
-        end
+        redirect_if_not_logged_in
+        erb :"blog_posts/new"
     end
 
     # CREAT new blog post from current logged in user
     post '/blog_posts' do
-        if logged_in?
-            blog_post = current_user.blog_posts.build(params)
-            blog_post.save
-            redirect '/blog_posts'
-        else
-            redirect '/login'
-        end
+        redirect_if_not_logged_in
+        blog_post = current_user.blog_posts.build(params)
+        blog_post.save
+        redirect '/blog_posts'
     end
 
     # READ display one particular blog post show page
     get '/blog_posts/:id' do
-        if logged_in?
-            @blog_post = BlogPost.find(params["id"])
-            erb :"blog_posts/show"
-        else 
-            redirect '/login'
-        end
+        redirect_if_not_logged_in
+        @blog_post = BlogPost.find(params["id"])
+        erb :"blog_posts/show"
     end
 
     # UPDATE display form
     get '/blog_posts/:id/edit' do
-        if logged_in?
-            @blog_post = BlogPost.find(params["id"])
-            erb :"blog_posts/edit"
-        else 
-            redirect '/login'
-        end
+        @blog_post = BlogPost.find(params["id"])
+        redirect_if_not_authorized
+        erb :"blog_posts/edit"
     end
 
     # UPDATE 
@@ -60,17 +49,25 @@ class BlogPostsController < ApplicationController
     # end
 
     post '/blog_posts/:id' do
-        if logged_in?
-            @blog_post = BlogPost.find(params["id"])
-            @blog_post.update(params["user"])
-            redirect "/blog_posts/#{@blog_post.id}"
-        else 
-            redirect '/login'
-        end
+        @blog_post = BlogPost.find(params["id"])
+        redirect_if_not_authorized           
+        @blog_post.update(params["user"])
+        redirect "/blog_posts/#{@blog_post.id}"
       end
 
 
     delete '/blog_posts:id' do
-        
+        @blog_post = BlogPost.find(params["id"])
+        redirect_if_not_authorized
+        @blog_post.destroy
+        redirect '/blog_posts'
+    end
+
+    private
+
+    def redirect_if_not_authorized
+        if @blog_post.user != current_user
+            redirect '/blog_posts'
+        end
     end
 end
