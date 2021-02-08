c04bebb0e839a9dac638ceac3bca236a89dd72ea
Cassandra <cparisi1290@gmail.com>
2021-02-08 16:07:31 -0500

    redirect user if not logged in method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
     get '/users/:id' do
         @user = User.find_by(id: session[:user_id])
+        redirect_if_not_logged_in
         # find user id to display their show page
         erb :"users/show"
     end
