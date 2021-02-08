7eb6a5a14d1d9e40695561e57ac796b32cd9fad6
Cassandra <cparisi1290@gmail.com>
2021-02-08 18:01:18 -0500

    update spelling of category_id

--- db/migrate/20210208211322_create_items.rb
+++ db/migrate/20210208211322_create_items.rb
@@ -3,7 +3,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
     create_table :items do |t|
       t.string :name
       t.integer :box_id
-      t.integer :cateogry_id
+      t.integer :category_id
       t.integer :user_id
       
       t.timestamps null: false
