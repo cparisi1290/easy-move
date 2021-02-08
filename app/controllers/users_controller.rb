c1c4952199e92c3b5800508a2361c95425041a1f
Cassandra <cparisi1290@gmail.com>
2021-02-08 09:54:47 -0500

    change how to find user show page from their params to user session id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
     get '/users/:id' do
-        @user = User.find(params["id"])
+        @user = User.find_by(id: session[:user_id])
         # find user id to display their show page
         erb :"users/show"
     end
