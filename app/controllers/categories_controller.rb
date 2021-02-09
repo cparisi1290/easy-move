b2a1e0dadf579dd6222b9acb32bb1361c9a4a627
Cassandra <cparisi1290@gmail.com>
2021-02-09 15:04:50 -0500

    change order of new and show routes, create edit route to render edit erb & form

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -12,18 +12,24 @@ class CategoriesController < ApplicationController
         erb :"categories/new"
     end
 
-    get '/categories/:id' do
-        redirect_if_not_logged_in
-        find_category
-        erb :"categories/show"
-    end
-
     post '/categories' do
         redirect_if_not_logged_in
         @category = Category.new(params)
         @category.user_id = current_user.id
         @category.save
         redirect "/categories/#{@category.id}"
+    end
+
+    get '/categories/:id' do
+        redirect_if_not_logged_in
+        find_category
+        erb :"categories/show"
+    end
+
+    get '/categories/:id/edit' do 
+        redirect_if_not_logged_in
+        find_category
+        erb :"categories/edit"
     end
 
     private
