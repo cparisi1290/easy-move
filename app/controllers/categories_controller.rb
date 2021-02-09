9d9b333165486c248522cbc76ebd3a0717320031
Cassandra <cparisi1290@gmail.com>
2021-02-09 15:00:07 -0500

    add login authorization method to category show route, create category new route, category show route, and category post route

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,13 +2,28 @@ class CategoriesController < ApplicationController
 
 
     get '/users/show' do
+        redirect_if_not_logged_in
         @categories = Category.all
         redirect "/users/#{current_user.id}"
     end
 
+    get '/categories/new' do
+        redirect_if_not_logged_in
+        erb :"categories/new"
+    end
+
     get '/categories/:id' do
+        redirect_if_not_logged_in
         find_category
         erb :"categories/show"
+    end
+
+    post '/categories' do
+        redirect_if_not_logged_in
+        @category = Category.new(params)
+        @category.user_id = current_user.id
+        @category.save
+        redirect "/categories/#{@category.id}"
     end
 
     private
