dd5d6422195db46326a249883475de6a36459c24
Cassandra <cparisi1290@gmail.com>
2021-02-09 11:12:27 -0500

    add user id to boxes and categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_211335) do
+ActiveRecord::Schema.define(version: 2021_02_09_160807) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
@@ -24,12 +24,14 @@ ActiveRecord::Schema.define(version: 2021_02_08_211335) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "items", force: :cascade do |t|
