11359400404dc73698c396896d492c458e10051e
Cassandra <cparisi1290@gmail.com>
2021-02-09 11:27:10 -0500

    change associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
     
     has_many :blog_posts
     has_many :items
-    has_many :categories, through: :items
-    has_many :boxes, through: :items
+    # has_many :categories, through: :items
+    has_many :categories
+    # has_many :boxes, through: :items
+    has_many :boxes
 end
