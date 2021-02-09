bee97c09135b85aa73f2c8fbf46c485e8690b53d
Cassandra <cparisi1290@gmail.com>
2021-02-09 14:51:59 -0500

    add category index page to user dashboard, create show page for one category, create method to refactor finding category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,19 @@
 class CategoriesController < ApplicationController
-    
+
+
+    get '/users/show' do
+        @categories = Category.all
+        redirect "/users/#{current_user.id}"
+    end
+
+    get '/categories/:id' do
+        find_category
+        erb :"categories/show"
+    end
+
+    private
+
+    def find_category
+        @category = Category.find(params["id"])
+    end
 end
