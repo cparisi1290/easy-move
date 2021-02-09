31f4c4c386b1bdb36043b7d40ce3128ca117a0f4
Cassandra <cparisi1290@gmail.com>
2021-02-09 11:26:42 -0500

    add log in authorization to current routes, create private find specific box method, create box show route bu using private method and redirect to box show page

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -1,18 +1,33 @@
 class BoxesController < ApplicationController
 
     get '/users/show' do
+        redirect_if_not_logged_in
         @boxes = Box.all
         redirect "/users/#{current_user.id}"
     end
 
     get '/boxes/new' do
+        redirect_if_not_logged_in
         erb :"boxes/new"
     end
 
     post '/boxes' do
+        redirect_if_not_logged_in
         @box = Box.new(params)
         @box.user_id = current_user.id
         @box.save
         redirect "/users/#{current_user.id}"
+    end
+
+    get '/items/:id' do
+        redirect_if_not_logged_in
+        find_box
+        erb :"boxes/show"
+    end
+
+    private
+
+    def find_box
+        @box = Box.find(params["id"])
     end
 end
