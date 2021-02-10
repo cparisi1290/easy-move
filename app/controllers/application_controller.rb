c7c368000812a4d82ace47ed1b30db453f06862a
Cassandra <cparisi1290@gmail.com>
2021-02-10 10:01:18 -0500

    change name of tab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, "easy-move"
+    set :session_secret, "ez-move"
   end
 
   get "/" do
