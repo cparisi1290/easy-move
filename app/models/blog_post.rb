c13b7d05400ea7ba693274977cd80fc859d31940
Cassandra <cparisi1290@gmail.com>
2021-02-12 09:42:42 -0500

    validatations

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,6 +1,8 @@
 class BlogPost < ActiveRecord::Base
     belongs_to :user
 
+    validates_presence_of :title, :content
+
     def formatted_created_at
         self.created_at.strftime("%A, %d %b %Y %l:%M %p")
       end
