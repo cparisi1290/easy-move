29541f01bd5cfc046bcb07d5c470b8aefd92498e
Cassandra <cparisi1290@gmail.com>
2021-02-12 09:44:00 -0500

    validatations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+    validates_presence_of :name, :email, :password
+    validates_uniqueness_of :email
+    
     has_secure_password
     
     has_many :blog_posts
